82bc63b2cd0bc45335813ff7e72a8ecbbe7a17f3
Edward Marchant <ejmarchant@gmail.com>
2018-06-08 11:53:41 +0100

    Fix compilation error for tvOS target when using the metal renderer

--- cocos2d/Platforms/iOS/CCMetalView.m
+++ cocos2d/Platforms/iOS/CCMetalView.m
@@ -54,7 +54,9 @@
 		self.backgroundColor = nil;
 		self.contentScaleFactor = [UIScreen mainScreen].scale;
 		
+        #if TARGET_OS_IOS
 		self.multipleTouchEnabled = YES;
+        #endif
 	}
 
 	return self;
